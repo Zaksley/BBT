cbfb1b0d9a9da29a95beb5299fbdd338fb18939b
Zaksley <enzo8550@gmail.com>
2020-02-21 13:28:46 +0100

    Add testSafety to check all the roads in Bordeaux

--- src/testDeserialize.py
+++ src/testDeserialize.py
@@ -1,5 +1,7 @@
 import utils.graphserializer, random, folium, webbrowser
+import os
 import math
+from utils.graphes import Edge
 
 bxlat = 44.8333
 bxlon = -0.5667
@@ -27,7 +29,16 @@ print(f"Path found\nIt contains {len(path)} nodes\nIts lenght is {distance}m\nDr
 for i in range(len(path)-1):
     coord1 = path[i].getCoordinates()
     coord2 = path[i+1].getCoordinates()
-    map.add_child(folium.PolyLine([(coord1[0], coord1[1]), (coord2[0], coord2[1])], color='red'))
+    edge = Edge.between(path[i], path[i+1])
+
+    c = 'red'
+    if edge.getSafety() == "safe":  c = 'green'
+    elif edge.getSafety() == "normal":  c= 'blue'
+    elif edge.getSafety() == "unsafe": c='orange'
+    elif edge.getSafety() == "very_unsafe": c='black'
+        
+
+    map.add_child(folium.PolyLine([(coord1[0], coord1[1]), (coord2[0], coord2[1])], color=c))
 
 
 print("Reset graph")
@@ -36,6 +47,7 @@ for node in nodes:
     node.setDistance(math.inf)
 print("Done")
 
+"""
 print("Finding path with Dijkstra...")
 path = graph.pathDijkstra(start, end)
 distance = path[len(path)-1].getDistance()
@@ -45,6 +57,7 @@ for i in range(len(path)-1):
     coord1 = path[i].getCoordinates()
     coord2 = path[i+1].getCoordinates()
     map.add_child(folium.PolyLine([(coord1[0], coord1[1]), (coord2[0], coord2[1])]))
+"""
 
 print("Paths have been drawn")
 
@@ -52,4 +65,4 @@ print("Paths have been drawn")
 print("Start saving map...")
 map.save('./map.html')
 print("Map saved")
-webbrowser.open_new_tab('./map.html')
+webbrowser.open_new_tab('file://' + os.path.abspath('./map.html'))
