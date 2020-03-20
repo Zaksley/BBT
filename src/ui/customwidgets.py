017892f3f0e02275546a49402c6cbeefda6cf47c
pjdevs <pj33720@gmail.com>
2020-03-21 00:37:22 +0100

    First GUI functionalities

---
+++ src/ui/customwidgets.py
@@ -0,0 +1,96 @@
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import *
+import requests, threading
+
+def formatAdress(text):
+    adress = text.split(" ")
+    sep = "+"
+    return sep.join(adress)
+
+class SearchEdit(QLineEdit):
+    showMessageBox = pyqtSignal(str)
+
+    def __init__(self, parent=None):
+        super(SearchEdit, self).__init__(parent)
+
+        self.currentThread = None
+        self.model = QStandardItemModel()
+        self.selectedItem = None
+
+        completer = QCompleter()
+        completer.setCompletionMode(QCompleter.UnfilteredPopupCompletion)
+        completer.setCaseSensitivity(Qt.CaseInsensitive)
+        completer.setModel(self.model)
+        completer.activated[QModelIndex].connect(self.onSelectedItem)
+        self.setCompleter(completer)
+
+        self.showMessageBox.connect(self.onShowMessageBox)
+        
+    def onShowMessageBox(self, text):
+        QMessageBox.critical(self, "Error", text, QMessageBox.Ok)
+    
+    def textChanged(self, text):
+        self.selectedItem = None
+        super(SearchEdit, self).textChanged(text)
+
+    def onSelectedItem(self, modelIndex):
+        self.selectedItem = modelIndex.row()
+    
+    def getCoords(self):
+        if self.selectedItem != None:
+            return self.model.takeItem(self.selectedItem).data()
+        else: raise Exception("No adress selected")
+
+    def event(self, event):
+        if event.type() == QEvent.KeyPress and event.key() == Qt.Key_Tab:
+            self._launchThread()
+            return True
+
+        return super(SearchEdit, self).event(event)
+    
+    def _updateAdresses(self):
+        if self.text() == "":
+            return
+
+        self.model.clear()
+
+        try:
+            adresses = requests.get('https://nominatim.openstreetmap.org/search?q=' + formatAdress(self.text()) +'&format=json&countrycodes=fr').json()
+        except Exception as err:
+            self.showMessageBox.emit(str(err))
+            return
+
+        for adress in adresses:
+            item = QStandardItem()
+            item.setText(adress["display_name"])
+            item.setData((adress["lat"], adress["lon"]))
+            self.model.appendRow(item)
+        
+    def _launchThread(self):
+        if self.currentThread == None or not self.currentThread.is_alive():
+            self.currentThread = threading.Thread(target=self._updateAdresses)
+            self.currentThread.start()
+        
+class SafetySlider(QWidget):
+    def __init__(self, parent=None):
+        super(SafetySlider, self).__init__(parent)
+
+        self.layout = QHBoxLayout()
+
+        self.safeLabel = QLabel("Sur ", self)
+        self.fastLabel = QLabel(" Rapide", self)
+
+        self.slider = QSlider(Qt.Horizontal, self)
+        self.slider.setMinimum(0)
+        self.slider.setMaximum(20)
+        self.slider.setValue(10)
+
+        self.layout.addWidget(self.safeLabel)
+        self.layout.addWidget(self.slider)
+        self.layout.addWidget(self.fastLabel)
+
+        self.setLayout(self.layout)
+
+    def getValue(self):
+        return self.slider.value() * 0.05
